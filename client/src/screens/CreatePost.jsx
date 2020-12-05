9e3bfe2665b663961a1235dc07fedbf1135d799b
Kevin Lo <kevlo96@gmail.com>
2020-12-05 01:00:05 -0500

    finished post create

--- client/src/screens/CreatePost.jsx
+++ client/src/screens/CreatePost.jsx
@@ -48,8 +48,68 @@ function CreatePost(props) {
                 autoFocus
                 onChange={handleChange}
               />
+
+              <div className="dropdown">
+                <label className="label-cat">Select Category</label>
+                <select
+                  className="input-category"
+                  value={post.category}
+                  name="category"
+                  required
+                  onChange={handleChange}
+                >
+                  <option>Korean</option>
+                  <option>Japanese</option>
+                  <option>Hairstyle</option>
+                </select>
+              </div>
             </div>
 
+            <input
+              className="image-link"
+              placeholder="image-url"
+              value={post.image}
+              name="image"
+              required
+              onChange={handleChange}
+            />
+
+            <input
+              className="outfit-input"
+              placeholder="Outfit Name"
+              value={post.outfit}
+              name="outfit"
+              onChange={handleChange}
+            />    
+
+            <input
+              className="price-input"
+              placeholder="Price"
+              value={post.price}
+              name="price"
+              onChange={handleChange}
+            /> 
+            
+            <input
+              className="tags-input"
+              placeholder="tag"
+              value={post.tags}
+              name="tags"
+              onChange={handleChange}
+            />
+            
+            <textarea
+              className="content-input"
+              placeholder="Enter Text"
+              value={post.content}
+              name="content"
+              required
+              onChange={handleChange}
+            />
+
+            <button type="submit" className="submit-button">
+             Post
+            </button>
           </div>
         </form>
       </div>
