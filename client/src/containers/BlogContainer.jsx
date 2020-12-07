538a75775ccd8bee03323d4fe8ac7d4eb9557bc3
Kevin Lo <kevlo96@gmail.com>
2020-12-06 21:25:30 -0500

    blog container is now styled:

--- client/src/containers/BlogContainer.jsx
+++ client/src/containers/BlogContainer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import BlogImage from '../assets/images/blogimage.jpg'
+import "./BlogContainer.css"
 
 function BlogContainer(props) {
   return (
     <div className="blog-div">
       <img className="blog-image" src={BlogImage} alt="blog image" />
-      <h2 className="blog-title">Winter Fashion Tips</h2>
-      <p className="blog-text">When it comes to wearing a flirty dress in winter, layering is key. Pair a fit’n flare number with a cropped sweater, or throw an oversized cardi or pullover on top so that your lightweight dress peeks out at the bottom. Add sneakers or ankle boots to keep your feet warm.</p>
+      <div className="blog-content">
+        <h2 className="blog-title">Winter Fashion Tips</h2>
+        <p className="blog-text">When it comes to wearing a flirty dress in winter, layering is key. Pair a fit’n flare number with a cropped sweater, or throw an oversized cardi or pullover on top so that your lightweight dress peeks out at the bottom. Add sneakers or ankle boots to keep your feet warm.</p>
+      </div>
+      
     </div>
   );
 }
