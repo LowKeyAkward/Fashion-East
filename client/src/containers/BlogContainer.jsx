f5e9afbc3cd819c339b5822797d40d32f371c45d
Kevin Lo <kevlo96@gmail.com>
2020-12-06 21:38:59 -0500

    updated blog post style

--- client/src/containers/BlogContainer.jsx
+++ client/src/containers/BlogContainer.jsx
@@ -8,7 +8,21 @@ function BlogContainer(props) {
       <img className="blog-image" src={BlogImage} alt="blog image" />
       <div className="blog-content">
         <h2 className="blog-title">Winter Fashion Tips</h2>
-        <p className="blog-text">When it comes to wearing a flirty dress in winter, layering is key. Pair a fit’n flare number with a cropped sweater, or throw an oversized cardi or pullover on top so that your lightweight dress peeks out at the bottom. Add sneakers or ankle boots to keep your feet warm.</p>
+        <p className="blog-text">
+          When it comes to wearing a flirty dress in winter, layering is key.
+          Pair a fit’n flare number with a cropped sweater, or throw an 
+          oversized cardi or pullover on top so that your lightweight dress 
+          peeks out at the bottom. Add sneakers or ankle boots to keep your 
+          feet warm. Rock any strappy or sleeveless dress over a 
+          thin fitted turtleneck (bulky items will make your summery 
+          dress bulge) and pair with cool combat boots for a slightly 
+          grunge look. This is a staple Korean winter fashion look 
+          which allows for lots of creativity. Another great idea is 
+          to match a basic white shirt with a sleeveless white dress: 
+          it will give the illusion of a sophisticated one piece 
+          looking très chic. Layer with a smart trench coat and 
+          embellished boots.
+          </p>
       </div>
       
     </div>
